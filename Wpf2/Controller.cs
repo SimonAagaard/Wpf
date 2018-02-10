353a39333b1eab1e42e5ff058c0f80bd6e22caa4
Simon Aagaard <simo136g@edu.eal.dk>
2018-02-10 13:40:57 +0100

    Add project files.

---
+++ Wpf2/Controller.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf2
+{
+    class Controller
+    {
+        private static Controller instance;
+        private Repository repository;
+        public Person CurentPerson { get; private set; }
+        public int PerconCount { get; private set; }
+        public int PersonIndex { get; private set; }
+
+        public Controller()
+        {
+            CurentPerson = null;
+            repository = new Repository();
+            PerconCount = 0;
+            PersonIndex = -1;
+        }
+        public static Controller GetInstance()
+        {
+            if (instance== null)
+            {
+                instance = new Controller();
+            }
+            return instance;
+        }
+
+        public void AddPerson()
+        {
+            Person percon = new Person();
+            CurentPerson = percon;
+            repository.AddPerson(percon);
+            PerconCount = repository.PersonList.Count();
+            PersonIndex = PerconCount-1;
+        }
+
+        public void DeletePerson()
+        {
+            if (CurentPerson != null)
+            {
+                repository.RemovePerson(CurentPerson);
+                
+                PerconCount = repository.PersonList.Count();
+                if (PersonIndex > 0)
+                {
+                    PersonIndex--;
+                }
+                else
+                {
+                    if (PerconCount == 0)
+                    {
+                        PersonIndex = -1;
+                    }
+                }
+
+                CurentPerson = repository.GetPersonAtIndex(PersonIndex);
+            }
+        }
+
+        public void NextPerson()
+        {
+            if (PersonIndex < PerconCount - 1)
+            {
+                PersonIndex++;
+                CurentPerson = repository.GetPersonAtIndex(PersonIndex);
+            }
+        }
+        public void PrevPerson()
+        {
+            if (PersonIndex > 0)
+            {
+                PersonIndex--;
+                CurentPerson = repository.GetPersonAtIndex(PersonIndex);
+            }
+        }
+
+
+    }
+}
---
+++ Wpf2/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            Controller.GetInstance().AddPerson();
+            Controller.GetInstance().CurentPerson.FirstName = Textbox1.Text;
+            Controller.GetInstance().CurentPerson.LastName = Textbox2.Text;
+            Controller.GetInstance().CurentPerson.Age = Convert.ToInt32(Textbox3.Text);
+            Controller.GetInstance().CurentPerson.TelephoneNr = Textbox4.Text;
+            PersonCount.Content = Controller.GetInstance().PerconCount;
+            Index.Content = Controller.GetInstance().PersonIndex;
+            
+            //Alternativ måde at lave personcount og index
+            //int percount = Convert.ToInt32(PersonCount.Content);
+            //PersonCount.Content = percount + 1;
+            //int perindex = Convert.ToInt32(PersonCount.Content);
+            //perindex = percount - 1;
+            //Index.Content = perindex + 1;
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            Controller.GetInstance().DeletePerson();
+            PersonCount.Content = Controller.GetInstance().PerconCount;
+            Index.Content = Controller.GetInstance().PersonIndex;
+            if (Controller.GetInstance().CurentPerson != null)
+            {
+                Textbox1.Text = Controller.GetInstance().CurentPerson.FirstName;
+                Textbox2.Text = Controller.GetInstance().CurentPerson.LastName;
+                Textbox3.Text = Controller.GetInstance().CurentPerson.Age.ToString();
+                Textbox4.Text = Controller.GetInstance().CurentPerson.TelephoneNr;
+            }
+        }
+
+        private void Up_Click(object sender, RoutedEventArgs e)
+        {
+            Controller.GetInstance().NextPerson();
+            Textbox1.Text = Controller.GetInstance().CurentPerson.FirstName;
+            Textbox2.Text = Controller.GetInstance().CurentPerson.LastName;
+            Textbox3.Text = Controller.GetInstance().CurentPerson.Age.ToString();
+            Textbox4.Text = Controller.GetInstance().CurentPerson.TelephoneNr;
+        }
+
+        private void Down_Click(object sender, RoutedEventArgs e)
+        {
+            Controller.GetInstance().PrevPerson();
+            Textbox1.Text = Controller.GetInstance().CurentPerson.FirstName;
+            Textbox2.Text = Controller.GetInstance().CurentPerson.LastName;
+            Textbox3.Text = Controller.GetInstance().CurentPerson.Age.ToString();
+            Textbox4.Text = Controller.GetInstance().CurentPerson.TelephoneNr;
+
+        }
+    }
+}
